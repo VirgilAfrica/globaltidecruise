61c9d9acc21c5d7bda1d206a5c290be8d4a80687
KingKiarie <kiarie7mbugua@gmail.com>
2025-02-11 11:59:25 +0300

    feat: added Authentication + passed test

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Globaltide.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:argon2_elixir, "~> 3.0"},
       {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.19"},
       {:phoenix_ecto, "~> 4.5"},
