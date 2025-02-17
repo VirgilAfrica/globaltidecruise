1b726d0b5f40a218a049205aa406b5fe2ad14ee6
Mgridgelabs <mainaianndungu@gmail.com>
2025-02-17 16:36:07 +0300

    `Added contact form functionality to ContactLive.FormComponent and updated ContactLive.Index to include the form`

--- lib/globaltide_web/live/contact_live/index.ex
+++ lib/globaltide_web/live/contact_live/index.ex
@@ -2,7 +2,7 @@ defmodule GlobaltideWeb.ContactLive.Index do
   use GlobaltideWeb, :live_view
 
   import GlobaltideWeb.ContactComponent
-  # import GlobaltideWeb.ContactFormComponent
+  alias GlobaltideWeb.ContactLive.FormComponent
 
   def mount(_params, _session, socket) do
     {:ok, assign(socket, is_open: false, current_index: 0)}
@@ -14,9 +14,18 @@ defmodule GlobaltideWeb.ContactLive.Index do
 
   def render(assigns) do
     ~H"""
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h1>Contact Us</h1>
+        </div>
+      </div>
+    </div>
+
     <.navbar is_open={@is_open} toggle_event="toggle-menu" />
     <.upper_section />
     <.contact_header />
+    <.live_component module={FormComponent} id="contact-form" />
     <.footer />
     """
   end
